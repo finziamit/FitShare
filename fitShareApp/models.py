1176210d52020cda7d7f9a1f2d88b266184eb737
Amit Finzi <finzi.amit@gmail.com>
2023-08-03 13:43:47 +0300

    Updating models methods

--- fitShareApp/models.py
+++ fitShareApp/models.py
@@ -38,15 +38,29 @@ class User(models.Model):
         user = User(name=name, password=password,gender=gender, weight=weight, height=height, email=email)
         user.save()
         return user
-
-    def add_training_program():
-        pass
-
-    def edit_training_program():
-        pass
     
-    def delete_training_program():
-        pass
+    @staticmethod
+    def get_user_by_email(email):
+        """
+        Method get email and return User object if exist, otherwise None
+        :param email: the user's email
+        :return: User object
+        """
+        user = User.objects.get(email=email)
+        return user
+    
+    @staticmethod
+    def delete_user_by_email(email):
+        """
+         Method to delete a user by his email
+        :param email: email to get the user
+        :return:  TRUE/FALSE if the user deleted/not
+        """
+        user = User.get_user_by_nickname(email=email)
+        if user not in User.objects.all():
+            raise User.DoesNotExist
+        else:
+            user.delete()
 
 
 class Exercise(models.Model):
@@ -61,11 +75,15 @@ class Exercise(models.Model):
     sets = models.IntegerField()
     reps = models.IntegerField()
 
-    def edit_sets():
-        pass
-
-    def edit_reps():
-        pass
+    def __str__(self):
+        return f"{self.__name}\t{self.__reps}x{self.__sets}"
+    
+    def edit_exercise(self, sets, reps):
+        if sets > 0:
+            self.__sets = sets
+        if reps > 0:
+            self.__reps = reps
+        self.save()
 
 
 class Training_Program(models.Model):
@@ -78,8 +96,5 @@ class Training_Program(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     excercises = models.ManyToManyField(Exercise)
 
-    def add_exercise():
-        pass
-
-    def delete_exercise():
-        pass
+    def __str__(self):
+        return f"{self.__user_id}'s Training program"
